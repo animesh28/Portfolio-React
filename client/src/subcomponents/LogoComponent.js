25ebd88faa5bc1999481b654838a8126ccdd0947
animesh28 <animesh.raj.om@gmail.com>
2022-08-18 01:23:04 +0530

    fix

--- client/src/subcomponents/LogoComponent.js
+++ client/src/subcomponents/LogoComponent.js
@@ -1,31 +1,29 @@
-import React from 'react'
-import styled from 'styled-components'
-import { darkTheme } from '../components/Themes'
-import logo from '../assets/Images/logo.png'
+import React from "react";
+import styled from "styled-components";
+import { darkTheme } from "../components/Themes";
+import logo from "../assets/Images/logo.png";
 
-
-
-function LogoComponent(props) {
+function LogoComponent() {
   return (
-    <Logo color={props.theme} className="logo">
-        <img src={logo} alt="" />
+    <Logo className="logo">
+      <img src={logo} alt="" />
     </Logo>
-  )
+  );
 }
 
 const Logo = styled.h1`
-    display: inline-block;
-    color: ${props => props.color === "dark" ? darkTheme.text : darkTheme.body };
-    font-family: 'Pacifico', cursive !important;
-    position: fixed;
-    top: 2rem;
-    left: 2rem;
-    z-index: 30;
+  display: inline-block;
+  color: ${(props) =>
+    props.color === "dark" ? darkTheme.text : darkTheme.body};
+  font-family: "Pacifico", cursive !important;
+  position: fixed;
+  top: 2rem;
+  left: 2rem;
+  z-index: 30;
 
-    img {
-      height: 75px;
+  img {
+    height: 75px;
+  }
+`;
 
-    }
-`
-
-export default LogoComponent
+export default LogoComponent;
